bd92b5fee5e73989a297f43c5311cd79be5f82b6
akmal2409 <akmalalikhujaev@gmail.com>
2020-12-24 09:12:44 +0100

    Initialized licensing service

---
+++ licensing-service/src/test/java/tech/talci/licensingservice/LicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.talci.licensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
