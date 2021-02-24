560447995f2980cfd04cf2023b38bedf3625e057
akmal2409 <akmalalikhujaev@gmail.com>
2021-02-24 17:50:23 +0100

    Added two clients OpenFeign and a classic one through rest template

--- licensing-service/src/main/java/tech/talci/licensingservice/LicenseServiceApplication.java
+++ licensing-service/src/main/java/tech/talci/licensingservice/LicenseServiceApplication.java
@@ -2,7 +2,6 @@ package tech.talci.licensingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,7 +10,6 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RefreshScope
-@EnableDiscoveryClient
 @EnableFeignClients
 public class LicenseServiceApplication {
 
@@ -24,4 +22,5 @@ public class LicenseServiceApplication {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
 }
