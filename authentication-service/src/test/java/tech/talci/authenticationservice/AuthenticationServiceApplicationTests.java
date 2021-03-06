c321962240b8562fde2b018aa7bf3f8064535ea4
akmal2409 <akmalalikhujaev@gmail.com>
2021-03-06 17:58:03 +0100

    Initialized Oauth2 authorization/authenctication service and set up basic configuration

---
+++ authentication-service/src/test/java/tech/talci/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.talci.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
